39c5c36f316f92eaba0dd28feb4823b0acddf47d
Derek Hamilton <dhamiltonstl@gmail.com>
2024-04-02 16:31:03 -0500

    part 1 completed

--- scrabble-scorer.js
+++ scrabble-scorer.js
@@ -33,7 +33,8 @@ function oldScrabbleScorer(word) {
 // don't change the names or your program won't work as expected. //
 
 function initialPrompt() {
-   console.log("Let's play some scrabble! Enter a word:");
+   console.log("Let's play some scrabble!");
+   console.log(oldScrabbleScorer(input.question("Enter a word: ")))
 };
 
 let simpleScorer;
